0f8be0ab791b929b70c3875f7dc3e07eec76ff33
Tanisha Sahni <ureachtanisha@gmail.com>
2024-03-13 17:53:48 -0400

    updated braces

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -31,10 +31,11 @@ string names[V] = {
 int minDistance(int dist[], bool sptSet[]) { 
     int min = INT_MAX, min_index; 
   
-    for (int v = 0; v < V; v++) 
-        if (sptSet[v] == false && dist[v] <= min) 
+    for (int v = 0; v < V; v++) {
+        if (sptSet[v] == false && dist[v] <= min) {
             min = dist[v], min_index = v; 
-  
+        }
+    }
     return min_index; 
 }
 
